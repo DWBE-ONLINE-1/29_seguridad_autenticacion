8a68d5e34f0258729e9ef89e3fa094a66e1e1562
abdon.flores <abdon.flores@globant.com>
2021-07-21 20:53:31 -0500

    Actividad programo

---
+++ Programo/server.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:6000'];
+let corsOptions = {
+	origin: 'http://localhost:5500',
+	optionsSuccessStatus: 200 // For legacy browser support
+};
+
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+})
+
+const app = express();
+// app.use('/', express.static(path.join(__dirname, 'static')));
+// app.use(bodyParser.json());
+app.use(express.json());
+
+// app.use(cors(corsOptions));
+
+// http://localhost:9999/api/change-password
+app.post('/api/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+
+		const _id = user.id
+
+		const password = await bcrypt.hash(plainTextPassword, 10)
+
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'jwt error' })
+	}
+})
+
+// http://localhost:9999/api/login
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+})
+
+// http://localhost:9999/api/register
+app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
+
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
+})
+
+app.listen(9999, () => {
+	console.log('Server up at 9999')
+})
+
+/*
+{
+	status: 500,
+	message_status: 'SERVER_ERROR',
+	message: 'info para el usuario',
+	details: [{}] || {}
+}
+*/
---
+++ Programo/static/js/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('reg-form')
+form.addEventListener('submit', registerUser)
+
+async function registerUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const result = await fetch('http://localhost:9999/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'json-token': 'gffgfgfg'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then((res) => res.json());
+
+    if (result.status === 'ok') {
+        // everythign went fine
+        alert('Success');
+    } else {
+        alert(result.error);
+    }
+}
